59446cee422cf0aed7d4cd9d24c7b7ce1fe7d336
elias-ka <48407293+elias-ka@users.noreply.github.com>
2024-09-13 13:51:28 +0300

    Done

---
+++ app/src/main/java/com/example/theming/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.theming
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.theming.ui.theme.ThemingTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ThemingTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    MyApp(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp(modifier: Modifier = Modifier) {
+    val appModifier = Modifier
+        .fillMaxWidth()
+        .padding(8.dp)
+    Column(verticalArrangement = Arrangement.spacedBy(16.dp), modifier = modifier) {
+        Text("My title", style = MaterialTheme.typography.headlineSmall, modifier = appModifier)
+        OutlinedTextField(value = "", onValueChange = {}, modifier = appModifier)
+        Button(onClick = {}, modifier = appModifier) {
+            Text("Submit")
+        }
+    }
+}
